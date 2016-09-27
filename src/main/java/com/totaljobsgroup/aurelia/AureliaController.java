08908fa6ef6f8e7b14010f9e4acc0cd1d5d9186c
Michal Przysucha <Michal.Przysucha@totaljobsgroup.com>
2016-09-27 15:33:08 +0200

    changes logging level from DEBUG to INFO

--- src/main/java/com/totaljobsgroup/aurelia/AureliaController.java
+++ src/main/java/com/totaljobsgroup/aurelia/AureliaController.java
@@ -6,6 +6,12 @@ import com.totaljobsgroup.aurelia.model.AtsResponseHeader;
 import com.totaljobsgroup.aurelia.model.Attachment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +19,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@EnableAutoConfiguration
+@SpringBootApplication
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,
+//        RabbitAutoConfiguration.class,
+//        HibernateJpaAutoConfiguration.class,
+//        JpaRepositoriesAutoConfiguration.class,
+//        DataSourceAutoConfiguration.class,
+//        DataSourceTransactionManagerAutoConfiguration.class})
 public class AureliaController {
 
     @ResponseBody
     @RequestMapping("/")
     String home() {
+        System.out.println(System.getProperty("logging.level.org.springframework"));
+        System.out.println(System.getenv("logging.level.org.springframework"));
         return "Hello World! My name is Aurelia. I'm the Applicant Tracking System.";
     }
 
